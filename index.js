7f868d2f5c64338bb28721896cddc60ba1c31129
NoorFatima1818 <159902924+NoorFatima1818@users.noreply.github.com>
2024-03-23 04:42:15 -0700

    updated number limit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
-const randomNumber = Math.floor(Math.random() * 5 + 1);
+const randomNumber = Math.floor(Math.random() * 7 + 1);
 const answers = await inquirer.prompt([
     {
         name: "userGuessedNumber",
         type: "number",
-        message: "Please guess a number from 1 to 5 :",
+        message: "Please guess a number from 1 to 7 :",
     }
 ]);
 if (answers.userGuessedNumber === randomNumber) {
--- index.ts
+++ index.ts
@@ -4,14 +4,14 @@
 
 import inquirer from "inquirer";
 
-const randomNumber = Math.floor(Math.random() *5 + 1 ) ;
+const randomNumber = Math.floor(Math.random() *7 + 1 ) ;
 
 const answers = await inquirer.prompt([
 
     { 
         name : "userGuessedNumber",
         type : "number",
-        message : "Please guess a number from 1 to 5 :", 
+        message : "Please guess a number from 1 to 7 :", 
     }
 
 
